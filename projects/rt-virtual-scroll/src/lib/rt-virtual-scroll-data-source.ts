f0f0b20efaf8fa066a8af3f8e9327b614e896dcd
Andriypodolian <50823653+Andriypodolian@users.noreply.github.com>
2023-08-16 12:23:17 +0300

    #6 Tweak interface

--- projects/rt-virtual-scroll/src/lib/rt-virtual-scroll-data-source.ts
+++ projects/rt-virtual-scroll/src/lib/rt-virtual-scroll-data-source.ts
@@ -1,23 +1,33 @@
 import {CollectionViewer, DataSource} from '@angular/cdk/collections';
 import {BehaviorSubject, first, Observable, Subject, Subscription} from 'rxjs';
 
+export interface RtVirtualScrollDataSourceModel<T> {
+    data: T[];
+    total: number;
+    nextPageUrl: string;
+}
+
 export class RtVirtualScrollDataSource<T> extends DataSource<T | undefined> {
 
-    public triggerPageChange: Subject<number> = new BehaviorSubject<number>(null);
+    public triggerPageChange: Subject<{ page: number, nextApi: string }> = new BehaviorSubject<{
+        page: number,
+        nextApi: string
+    }>(null);
     private _pageSize = 10;
     private _length = 100000;
-    private _cachedData = Array.from<T>({length: this._length});
+    private _cachedData: T[];
     private _fetchedPages = new Set<number>();
     private _dataStream: BehaviorSubject<(T | undefined)[]> = new BehaviorSubject<(T | undefined)[]>([]);
+    private _nextPageUrl: string;
+
     private readonly _subscription = new Subscription();
 
-    constructor(data: Observable<(T | undefined)[]>, pageSize = 10, length = 100) {
+    constructor(pageSize = 10, length = 100) {
         super();
         this._pageSize = pageSize;
         this._length = length;
-        this._cachedData = Array.from<T>({length: this._length});
-        this._dataStream.next(this._cachedData);
-        this.mergeDate(data, 0);
+
+
     }
 
 
@@ -41,29 +51,36 @@ export class RtVirtualScrollDataSource<T> extends DataSource<T | undefined> {
         this._subscription.unsubscribe();
     }
 
-    appendItems(data: Observable<T[]>, page: number) {
-        if (page > 0) {
-            this.mergeDate(data, page);
+    appendItems(data: Observable<RtVirtualScrollDataSourceModel<T>>, page: number) {
+        this.mergeDate(data, page);
+    }
+
+    resetData() {
+        this._cachedData = Array.from<T>({length: 10});
+        this._dataStream.next(this._cachedData);
+    }
+
+    private mergeDate(data: Observable<RtVirtualScrollDataSourceModel<T>>, page: number) {
+        if (this._cachedData) {
+            data.pipe(first()).subscribe({
+                next: (v) => {
+                    if (page === 0) {
+                        this._length = v.total;
+                        this._cachedData = Array.from<T>({length: this._length});
+                    }
+                    this._nextPageUrl = v.nextPageUrl;
+                    this._cachedData.splice(
+                        page * this._pageSize,
+                        this._pageSize,
+                        ...v.data,
+                    );
+                    this._dataStream.next(this._cachedData);
+                },
+            });
+        } else {
+            this._cachedData = Array.from<T>({length: 10});
+            this._dataStream.next(this._cachedData);
         }
-    }
-
-    setData(data: Observable<T[]>) {
-        data.pipe(first()).subscribe({
-            next: (v) => this._dataStream.next(v),
-        });
-    }
-
-    private mergeDate(data: Observable<T[]>, page: number) {
-        data.pipe(first()).subscribe({
-            next: (v) => {
-                this._cachedData.splice(
-                    page * this._pageSize,
-                    this._pageSize,
-                    ...v,
-                );
-                this._dataStream.next(this._cachedData);
-            },
-        });
     }
 
     private _getPageForIndex(index: number): number {
@@ -75,7 +92,7 @@ export class RtVirtualScrollDataSource<T> extends DataSource<T | undefined> {
             return;
         }
         this._fetchedPages.add(page);
-        this.triggerPageChange.next(page);
+        this.triggerPageChange.next({page, nextApi: this._nextPageUrl});
 
     }
 }
