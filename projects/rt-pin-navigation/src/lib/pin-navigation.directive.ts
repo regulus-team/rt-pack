f2b0f76142d7904b059f13000e4f8ce66459659a
Andriypodolian <50823653+Andriypodolian@users.noreply.github.com>
2023-12-27 12:04:51 +0200

    Tweak rt-pin-navicagion, add constrain for firefox browser

--- projects/rt-pin-navigation/src/lib/pin-navigation.directive.ts
+++ projects/rt-pin-navigation/src/lib/pin-navigation.directive.ts
@@ -1,4 +1,5 @@
 import {AfterViewInit, Directive, ElementRef, Renderer2} from '@angular/core';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Directive({
   selector: '[rtPinNavigation]',
@@ -9,13 +10,19 @@ export class RtPinNavigationDirective implements AfterViewInit {
   constructor(
     private el: ElementRef,
     private renderer: Renderer2,
+    private deviceService: DeviceDetectorService,
   ) {
   }
 
   ngAfterViewInit(): void {
+    if (this.deviceService.browser === 'Firefox') {
+      return;
+    }
+
     this.renderer.setStyle(this.el.nativeElement, 'position', 'sticky');
     this.renderer.setStyle(this.el.nativeElement, 'top', '0');
     this.renderer.setStyle(this.el.nativeElement, 'z-index', '1000');
+
   }
 
 }
