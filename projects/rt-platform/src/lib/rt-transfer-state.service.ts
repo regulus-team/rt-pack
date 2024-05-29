aed64401672f396bbb8f2d496ee3424ae4c80397
andriy_podolian <georgboosh@gmail.com>
2024-05-29 12:49:41 +0300

    Update rt-platform

--- projects/rt-platform/src/lib/rt-transfer-state.service.ts
+++ projects/rt-platform/src/lib/rt-transfer-state.service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@angular/core';
+import {Injectable, StateKey, TransferState} from '@angular/core';
 import {RtPlatformService} from './rt-platform.service';
-import {StateKey, TransferState} from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RtTransferStateService {
-  constructor(private platform: RtPlatformService, private transfer: TransferState) {}
+  constructor(private platform: RtPlatformService, private transfer: TransferState) {
+  }
 
   private getKey(name: string): StateKey<string> {
     return `state-${name}` as StateKey<string>;
